83cf010a59e753951c5f900a7efa2f40b93460e5
WillTheCodeWork <165967401+WillTheCodeWork@users.noreply.github.com>
2024-06-23 08:54:39 -0400

    edited license to gpl-3

--- src/main/java/me/will/scythe/item/ModItems.java
+++ src/main/java/me/will/scythe/item/ModItems.java
@@ -1,4 +1,51 @@
 package me.will.scythe.item;
 
-public class items {
+import me.will.scythe.Scythe;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Scythe.MODID);
+    public static final RegistryObject<Item> scythe_item =
+            ITEMS.register("scythe", () -> new SwordItem(new Tier() {
+                @Override
+                public int getUses() {
+                    return 200;
+                }
+
+                @Override
+                public float getSpeed() {
+                    return 0.1f;
+                }
+
+                @Override
+                public float getAttackDamageBonus() {
+                    return 0;
+                }
+
+                @Override
+                public int getLevel() {
+                    return 0;
+                }
+
+                @Override
+                public int getEnchantmentValue() {
+                    return 0;
+                }
+
+                @Override
+                public Ingredient getRepairIngredient() {
+                    return null;
+                }
+            }, 999999999, -1f, new Item.Properties()));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
